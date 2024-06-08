4f5ecd556bcbe9e3ecd97339c54d41e6778905e2
Prakash Sharma <ps4687675@gmail.com>
2024-06-08 17:32:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+let Randnum = document.getElementById("RandNum");
+let num =Math.floor(Math.random()*5)+1;
+let GuessNum = document.getElementById("GuessNum");
+let Result = document.getElementById("result");
+let gameChance = 5;
+let GameChance = document.getElementById("gameChance");
+GameChance.innerHTML="You have "+gameChance+" life left";
+function generateRandomNumber() {
+    num = Math.floor(Math.random()*5)+1;
+    console.log(num);
+    
+    
+    
+    
+    if(Number(GuessNum.value)===num){
+        Result.innerHTML="Congratulation";
+    }
+    else{
+        gameChance--;
+        GameChance.innerHTML="You have "+gameChance+" life left";
+        
+    }
+
+    if(gameChance==0){
+        document.getElementById("gameover").style= "display: block";
+        document.getElementById("game").style= "display: none"
+    }
+    GuessNum.value='';
+    Randnum.innerHTML="Actual Value was "+num
+    return num;
+}
+
+
+
+function hint(){
+    if(GuessNum.value!=''){
+        if(Number(GuessNum.value)<num){
+            Result.innerHTML="Your Guessed value is Smaller";
+        }
+        else if(Number(GuessNum.value)>num){
+            Result.innerHTML="Your Guessed Value is Greater";
+        }
+    }
+    else{
+        alert('Enter Value')
+    }
+}
+
